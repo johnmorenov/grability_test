4fba22443df8ccdc1e874969d0dc0f18575ad5e9
John William Moreno Vergel <johnmorenov@yahoo.com>
2016-06-17 12:46:19 -0500

    Update #2, new test case

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -5,17 +5,17 @@ namespace App\Http\Controllers;
 use Request;
 use Response;
 
-use App\Http\Requests;
 use App\Classes\QueryProcessingClass;
-use App\Cube;
 
+use App\Http\Requests;
 use App\Http\Controllers\CubeCalculationsController;
+use App\Http\Controllers\StoreDataController;
 
 class QueryController extends queryValidationController
 {
 	var $cubeDimensions, $N;
 	var $query;
-
+    
     public function queryResult() {
     	$aPost = Request::all();
     	
@@ -28,28 +28,17 @@ class QueryController extends queryValidationController
         if($aResult['result'] === 'ok') {
             $oQuery = new QueryProcessingClass();
             $aParts = $oQuery->queryToArray($this->query);
-
-            // CACHE de datos
-            // Las queries de tipo UPDATE se almacenaran en BD para consultarla luego con las QUERY
+            
             if ($aParts['tipo'] === 'UPDATE') {
-                $oSearch = Cube::where('x1', $aParts['x1'])->where('y1', $aParts['y1'])->where('z1', $aParts['z1'])->first();
-                if($oSearch) {
-                    $status = $oSearch->update($aParts);
-                } else {
-                    $status = Cube::create($aParts);
-                }
-
-                if($status) {
-                    $aResult['result'] = 'Cube('.$aParts['x1'].', '.$aParts['y1'].', '.$aParts['z1'].') = '.$aParts['W'].', ok!';
-                } else {
-                    $aResult['result'] = 'Error al almecenar valor';
-                }
+                // CACHE de datos
+                // Las queries de tipo UPDATE se almacenaran en BD para consultarla luego con las QUERY
+                $aResult['result'] = StoreDataController::storeData($aParts);
             }
             else if ($aParts['tipo'] === 'QUERY') {
                 $aResult['result'] = 'Sum = '.CubeCalculationsController::cubeSummation($this->N, $aParts);
             }
         }
-
+        
     	return Response::json( $aResult );
     }
 }
---
+++ app/Http/Controllers/StoreDataController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Request;
+use Response;
+
+use App\Http\Requests;
+
+use App\Cube;
+
+class StoreDataController extends Controller
+{
+    public static function storeData($aParts) {
+        $sResponse = "";
+        $sResultLabel = 'Cube('.$aParts['x1'].', '.$aParts['y1'].', '.$aParts['z1'].') = '.$aParts['W'].'';
+
+        $oSearch = Cube::where('x1', $aParts['x1'])->where('y1', $aParts['y1'])->where('z1', $aParts['z1'])->first();
+
+        if($oSearch) {
+            if($oSearch->update($aParts)) {
+                $sResponse = $sResultLabel.', updated!';
+            } else {
+                $sResponse = 'Error al actualizar valor';
+            }
+        } else {
+            if(Cube::create($aParts)) {
+                $sResponse = $sResultLabel.', ok!';
+            } else {
+                $sResponse = 'Error al guardar valor';
+            }
+        }
+
+    	return $sResponse;
+    }
+}
